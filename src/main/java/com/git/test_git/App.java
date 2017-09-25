390a2ac6aeb78f671a41a28d063a2410663a1772
amitverma-roposo <amit.verma@roposo.com>
2017-09-25 11:01:07 +0530

    master 2nd commit

--- src/main/java/com/git/test_git/App.java
+++ src/main/java/com/git/test_git/App.java
@@ -8,8 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
+    	int master = 10;
     	int a= 0;
         System.out.println( "Hello World!" );
         System.out.println(a);
+        System.out.println(master);
     }
 }
