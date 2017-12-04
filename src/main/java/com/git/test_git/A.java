379e0beff42f669b10b26e6cb879fc545a05135d
amitverma-roposo <amit.verma@roposo.com>
2017-12-04 11:12:12 +0530

    ============

--- src/main/java/com/git/test_git/A.java
+++ src/main/java/com/git/test_git/A.java
@@ -1,8 +1,15 @@
 package com.git.test_git;
 
-public class A {
+import java.util.Date;
 
-	private void a(){
-		
+public class A {
+	
+	
+
+	public static  void main(String[] a ){
+		String str = "a_bcd_hjk";
+		String s = "_";
+		System.out.println(str.substring(0, str.indexOf("_")));
+		System.out.println(str.contains(s));
 	}
 }
